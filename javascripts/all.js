8183d3c6fb8de3dde9d43e71dab96fe49b267398
Landy510 <randychen510@gmail.com>
2021-05-29 17:57:25 +0800

    Update 2021-05-29T09:57:22.715Z

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $('.content-QA-btn').on('click', function (e) {
     $(this).parent().find('.content-QA-description').slideToggle()
+    $(this).parent().toggleClass('QAList-option-active')
     $(this).find('.fa-chevron-down').toggleClass('content-QA-arrow-active')
   })
 });
